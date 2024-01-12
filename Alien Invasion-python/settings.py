96d37900e1c8f016a52d2db3da3beafdcbfe06d0
Birdy <2944006086@qq.com>
2024-01-13 00:19:00 +0800

    alien invasion game in python (unfinished)

---
+++ Alien Invasion-python/settings.py
@@ -0,0 +1,13 @@
+class Settings:
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        # 设置背景颜色 --浅灰色
+        self.bg_color = (230, 230, 230)
+
+        # 飞船设置
+        self.ship_speed = 1.5
+
---
+++ Alien Invasion-python/ship.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Ship:
+    """管理飞船的类"""
+
+    """ai_game 指向alien_invasion实"""
+    def __init__(self, ai_game):
+        """初始化飞船并设置其初始位置"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # 将每艘新飞船都放在屏幕底部的中央
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # 在飞船的属性 x 中存储小数值
+        self.x = float(self.rect.x)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动调整飞船的位置"""
+        # 保证不越界
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
